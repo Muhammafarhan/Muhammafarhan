59f93af61a5a25af4686d55e9c963f40ddef6375
Muhammafarhan <farhan290993@gmail.com>
2026-02-17 09:04:48 +0700

    Remove extra newline in index.php
    "Tambah file index.php pertama"

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// Kode PHP Pertama di GitHub
+echo "<h1>Halo! Ini PHP dari GitHub saya!</h1>";
+echo "<p>Nama saya Muhammafarhan, sedang belajar PHP dan Database.</p>";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP dari GitHub</title>
+</head>
+<body>
+    <hr>
+    <p>Halaman ini dibuat di GitHub dan bisa di-host secara gratis!</p>
+</body>
+</html>
